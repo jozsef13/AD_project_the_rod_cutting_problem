b8584ac2312bd0b6709bd1ea7b6d5a114755c8c4
jozsef13 <38989437+jozsef13@users.noreply.github.com>
2018-05-19 19:44:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int rod_length, rod_pieces_prices[1000];
+    
+    read_rod_lenght(&rod_lenght);
+    read_rod_pieces_prices(rod_pieces_prices, rod_lenght);
+    printf("%d", rod_cutting_first_algorithm(rod_pieces_prices, rod_lenght));
+    
     return 0;
 }
