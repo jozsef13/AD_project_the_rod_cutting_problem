81017798051be0a87f6fc026ef59a11678f3503b
jozsef13 <38989437+jozsef13@users.noreply.github.com>
2018-05-27 13:19:15 +0300

    Update second_algorithm.c

--- second_algorithm.c
+++ second_algorithm.c
@@ -1,28 +1,23 @@
 #include "second_algorithm.h"
 #include "common_functions.h"
-///a second algorithm with an interative implementation of dynamic programming
-int rod_cutting_second_algorithm(int rod_pieces_prices[], int rod_lenght)
+///a second algorithm with a recursive implementation of dynamic programming
+int rod_cutting_second_algorithm(int rod_pieces_prices[], int rod_length)
 {
-    ///array used for calculatig the maximum prices you get when cutting the rod
-    int maximum_prices[1000];
     ///variable used to calculate the maximum value at each step
-    int max_value; 
-    int j, i;
-    ///initialization of the maximum price until the first cutting with 0
-    maximum_prices[1] = 0; 
-    ///algorithm implementation
-    for(j = 2; j <= rod_lenght; j++){
-        max_value = -1000;
-        ///calculation of the maximum value until we get to the j rod,in order to observ which is the best way to cut it       
-        for(i = 2; i <= j; i++){
-            if(max_value < rod_pieces_prices[i] + maximum_prices[j - i]){
-                max_value = rod_pieces_prices[i] + maximum_prices[j - i];
-            }
-        }
-        ///the maximum price until j is calculated and saved on the j position
-        maximum_prices[j] = max_value;
+    int max_value;
+    int i;
+
+    ///the stopping statement
+    if(rod_length <= 0){
+        return 0;
     }
 
-    return maximum_prices[rod_lenght];
-}
+    max_value = -1000;
+    ///algorithm implementation recursively
+    for(i = 1; i <= rod_length; i++){
+        max_value = maximum_value(max_value, rod_pieces_prices[i] + rod_cutting_second_algorithm(rod_pieces_prices, rod_length - i));
 
+    }
+
+    return max_value;
+}
