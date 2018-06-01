e25d14e8750669e7b89b841a2a2b21ba8daf2df2
jozsef13 <38989437+jozsef13@users.noreply.github.com>
2018-06-01 13:45:52 +0300

    Update second_algorithm.h

--- second_algorithm.h
+++ second_algorithm.h
@@ -1,2 +1,13 @@
+///\file second_algorithm.h
+///\brief C library for the second algorithm function
+///
+/// Created by Simon Jozsef-Gabriel on  28/05/18
+/// Implements the second algorithm of rod cutting problem
+
+#ifndef SECOND_ALGORITHM_H_
+#define SECOND_ALGORITHM_H_
+#define MIN -1000
+
 int rod_cutting_second_algorithm(int rod_pieces_prices[], int rod_length);
 
+#endif //SECOND_ALGORITHM_H
