8985abef3d75c27d790fa1b005d57621ee776e3b
jozsef13 <38989437+jozsef13@users.noreply.github.com>
2018-06-01 13:45:25 +0300

    Update first_algorithm.h

--- first_algorithm.h
+++ first_algorithm.h
@@ -1,2 +1,13 @@
+///\file first_algorithm.h
+///\brief C library for the first algorithm function
+///
+/// Created by Simon Jozsef-Gabriel on  28/05/18
+/// Implements the first algorithm of rod cutting problem
+
+#ifndef FIRST_ALGORITHM_H_
+#define FIRST_ALGORITHM_H_
+#define MIN -1000
+
 int rod_cutting_first_algorithm(int rod_pieces_prices[], int rod_length);
 
+#endif //FIRST_ALGORITHM_H
