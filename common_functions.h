17c95c123817f49ed8456e429b8de54866735707
jozsef13 <38989437+jozsef13@users.noreply.github.com>
2018-06-03 14:46:46 +0300

    Update common_functions.h

--- common_functions.h
+++ common_functions.h
@@ -10,5 +10,6 @@
 void read_rod_length(int *rod_length);
 void read_rod_pieces_prices(int rod_pieces_prices[], int rod_length);
 int maximum_value(int first_value, int second_value);
+int print_rod(int rod_length, int rod_pieces_prices[]);
 
 #endif // COMMON_FUNCTIONS_H
